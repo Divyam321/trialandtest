53cf85b6a760aae883567172913932a17ce74b38
Divyam321 <divyamrawat010@gmail.com>
2024-07-13 17:07:28 +0530

    first

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import express from 'express'
+
+const app= express()
+
+
+
+// app.get('/',(req,res)=>{
+    
+//     res.send("Server is ready");
+// })
+
+//get a list of 5 jokes
+
+app.get("/jokes",(req,res)=>{
+    const jokes=[
+        {
+            id:1,
+            title:"a joke",
+            content:"this is a joke"
+        },
+        {
+            id:2,
+            title:"a second joke",
+            content:"this is a another joke"
+        },
+        {
+            id:3,
+            title:"a third joke",
+            content:"this is a third joke joke"
+        }
+
+        
+    ]
+    res.send(jokes)
+
+})
+
+const PORT=process.env.PORT || 3000
+
+
+app.listen(PORT,()=>{
+    console.log(`Server at http://localhost:${PORT}`)
+})
