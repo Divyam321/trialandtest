8bb88620fb6083db30cdb0c49216360a1449f597
Divyam321 <divyamrawat010@gmail.com>
2024-07-13 18:37:07 +0530

    Updated settings and configurations

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://trialandtest-3.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
